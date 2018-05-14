1900ed62909cd4bfe89da9bd1e55b154ca43b6f9
Andy Strominger <andrew.strominger@spidasoftware.com>
2018-05-14 08:02:07 -0400

    Debug failing test

--- src/test/groovy/com/spidasoftware/mongodb/data/MongoDBDataAccessSpec.groovy
+++ src/test/groovy/com/spidasoftware/mongodb/data/MongoDBDataAccessSpec.groovy
@@ -26,6 +26,7 @@ class MongoDBDataAccessSpec extends Specification {
     void testGetNames() {
         when:
             List<Name> names = mongoDBDataAccess.getNames()
+            log.info("names.size() = ${names.size()} names = ${names}")
         then:
             names.size() == 20
             names.findAll { it.localPart == "location" }.size() == 1
